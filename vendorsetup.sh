0bec9cef2049020daf747187ff6da3d71e83d486
sairb1 <sairbgamer@gmail.com>
2025-04-17 09:38:52 +0530

    Initial commit for TWRP device tree

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_ossi-user
+add_lunch_combo omni_ossi-userdebug
+add_lunch_combo omni_ossi-eng
+add_lunch_combo twrp_RE5C6CL1-eng
